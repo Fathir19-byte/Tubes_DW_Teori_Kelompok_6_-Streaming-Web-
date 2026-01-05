47cf9090b4ced59378c7d0e91cbad46f6a74f9c2
Fathir19-byte <muhammadfathir1906@gmail.com>
2026-01-06 01:37:03 +0700

    Progress Dashboard

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
             id: 1,
             title: "Spy x Family Code: White", 
             year: "2023", 
+            duration: "1h 50m", 
             rating: "9.1/10",
             desc: "Perjalanan liburan keluarga Forger ke wilayah Frigis agar Anya bisa mencicipi makanan khas untuk memenangkan kompetisi memasak di Akademi Eden, namun liburan itu berubah menjadi misi penyelamatan dunia.",
             genres: ["Anime", "Action", "Comedy", "Family"], 
@@ -20,6 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
             id: 2,
             title: "Haikyu!! The Dumpster Battle", 
             year: "2024", 
+            duration: "1h 25m", 
             rating: "8.8/10",
             desc: "Pertarungan legendaris di tempat sampah! Karasuno vs Nekoma akhirnya bertemu dalam pertandingan resmi yang penuh emosi dan strategi.",
             genres: ["Anime", "Sports", "Drama"], 
@@ -32,6 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
             id: 3,
             title: "Agak Laen", 
             year: "2024", 
+            duration: "1h 59m", 
             rating: "7.9/10",
             desc: "Empat sekawan penjaga rumah hantu yang sepi pengunjung dan hampir bangkrut, yang akhirnya menemukan keuntungan tak terduga saat salah satu pengunjung meninggal karena kaget.",
             genres: ["Comedy", "Horror"], 
@@ -44,6 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
             id: 4,
             title: "Zootopia", 
             year: "2016", 
+            duration: "1h 48m", 
             rating: "8.0/10",
             desc: "Di kota hewan antropomorfik, Judy Hopps, seekor kelinci polisi pemula, harus bekerja sama dengan rubah penipu bernama Nick Wilde untuk memecahkan misteri konspirasi.",
             genres: ["Adventure", "Comedy", "Family"],
@@ -56,6 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
             id: 5,
             title: "Avengers: Endgame", 
             year: "2019", 
+            duration: "3h 1m", 
             rating: "8.4/10",
             desc: "Setelah peristiwa Thanos yang melenyapkan setengah populasi alam semesta, para Avengers yang tersisa harus berkumpul kembali dan melakukan perjalanan waktu.",
             genres: ["Action", "Adventure", "Sci-Fi"],
@@ -68,14 +73,148 @@ document.addEventListener('DOMContentLoaded', () => {
             id: 6,
             title: "F1",  
             year: "2025", 
+            duration: "2h 5m", 
             rating: "7.7/10", 
             desc: "Sonny Hayes, mantan pembalap Formula 1 yang sempat pensiun, kembali ke lintasan untuk menjadi mentor bagi rekan setimnya yang masih muda di tim APXGP.",
             genres: ["Action", "Drama", "Sport"],
             poster: "./Asset/PosterFilm/F1TheMovie.jpg",
             background: "./Asset/BackgroundFilm/F1TheMovieL.jpeg", 
-            trailerId: "h1QzGGfrsbk", // ID Trailer Resmi
+            trailerId: "h1QzGGfrsbk", 
             filmId: "h1QzGGfrsbk"
-        }
+        },
+        {
+            id: 7,
+            title: "Joker", 
+            year: "2019", 
+            duration: "2h 2m", 
+            rating: "8.4/10", 
+            desc: "Arthur Fleck, seorang komedian gagal yang merasa terasing di Kota Gotham, perlahan jatuh ke dalam kegilaan dan memicu revolusi kriminal yang mengubah nasib kota tersebut selamanya.",
+            genres: ["Action", "Drama", "Thriller"],
+            poster: "./Asset/PosterFilm/Joker.jpeg",
+            background: "./Asset/BackgroundFilm/Joker.jpg", 
+            trailerId: "zAGVQLHvwOY",
+            filmId: "zAGVQLHvwOY"
+        },
+        {
+            id: 8,
+            title: "Moana", 
+            year: "2016", 
+            duration: "1h 47m", 
+            rating: "7.6/10", 
+            desc: "Seorang remaja petualang berlayar dalam misi berbahaya untuk menyelamatkan rakyatnya, dibantu oleh demigod Maui, demi mengembalikan jantung Te Fiti dan memulihkan keseimbangan lautan.",
+            genres: ["Adventure","Comedy", "Family"],
+            poster: "./Asset/PosterFilm/Moana.jpeg",
+            background: "./Asset/BackgroundFilm/MoanaL.jpg", 
+            trailerId: "LKFuXETZUsI",
+            filmId: "LKFuXETZUsI"
+        },
+        {
+            id: 9,
+            title: "The Migration", 
+            year: "2023", 
+            duration: "1h 23m",
+            rating: "7.2/10", 
+            desc: "Keluarga bebek yang protektif akhirnya memutuskan untuk pergi berpetualang dan bermigrasi ke Jamaika, namun rencana mereka berantakan saat mereka tersesat di tengah hiruk-pikuk kota New York.",
+            genres: ["Adventure", "Comedy", "Family"],
+            poster: "./Asset/PosterFilm/The Migration Poster.webp",
+            background: "./Asset/BackgroundFilm/MigrationL.jpg", 
+            trailerId: "LZbTMiPpBIo",
+            filmId: "LZbTMiPpBIo"
+        },
+        {
+            id: 10,
+            title: "Fast & Furious 8", 
+            year: "2017", 
+            duration: "2h 16m",
+            rating: "6.7/10", 
+            desc: "Saat Dom dan Letty sedang menikmati bulan madu, seorang wanita misterius memaksa Dom untuk mengkhianati keluarganya dan kembali ke dunia kejahatan yang tidak bisa ia hindari.",
+            genres: ["Action", "Thriller"],
+            poster: "./Asset/PosterFilm/Fast & Furious 8.jpeg",
+            background: "./Asset/BackgroundFilm/Fast&Furious8L.jpg", 
+            trailerId: "NxhEZG0k9_w",
+            filmId: "NxhEZG0k9_w"
+        },
+        {
+            id: 11,
+            title: "Ra.One", 
+            year: "2011", 
+            duration: "2h 36m",
+            rating: "6.2/10", 
+            desc: "Seorang desainer game menciptakan tokoh penjahat super yang tak terkalahkan bernama Ra.One. Kekacauan terjadi ketika Ra.One melarikan diri ke dunia nyata, memaksa G.One sang pahlawan game untuk ikut keluar demi menyelamatkan keluarga penciptanya.",
+            genres: ["Action", "Adventure", "Sci-Fi"],
+            poster: "./Asset/PosterFilm/Ra_One (2011).jpeg",
+            background: "./Asset/BackgroundFilm/Ra_OneL.jpeg", 
+            trailerId: "prRdhKyGRm8",
+            filmId: "prRdhKyGRm8"
+        },
+        {
+            id: 12,
+            title: "Despicable Me 4", 
+            year: "2024", 
+            duration: "1h 34m",
+            rating: "6.3/10", 
+            desc: "Gru menyambut anggota baru di keluarganya, Gru Jr., yang berniat menyiksa ayahnya. Namun, keluarga mereka terpaksa melarikan diri ketika musuh baru, Maxime Le Mal, muncul untuk membalas dendam.",
+            genres: ["Adventure", "Comedy", "Family"],
+            poster: "./Asset/PosterFilm/Despicable-Me-4 Poster.jpg",
+            background: "./Asset/BackgroundFilm/Despicable-Me-4L.jpeg", 
+            trailerId: "qQlr9-rF32A",
+            filmId: "qQlr9-rF32A"
+        },
+        {
+            id: 13,
+            title: "The Dark Knight", 
+            year: "2008", 
+            duration: "2h 32m",
+            rating: "9.0/10", 
+            desc: "Batman, Letnan Gordon, dan Jaksa Wilayah Harvey Dent bersekutu untuk membongkar sindikat kriminal Gotham, namun mereka dihadang oleh dalang kriminal anarkis yang dikenal sebagai The Joker.",
+            genres: ["Action", "Crime", "Drama"],
+            poster: "./Asset/PosterFilm/theDarkKnight.jpg",
+            background: "./Asset/BackgroundFilm/theDarkKnightL.jpeg", 
+            trailerId: "EXeTwQWrcwY",
+            filmId: "EXeTwQWrcwY"
+        },
+        {
+            id: 14,
+            title: "How to Make Millions Before Grandma Dies", 
+            year: "2024", 
+            duration: "2h 7m",
+            rating: "8.2/10", 
+            desc: "Seorang pemuda rela berhenti bekerja demi merawat neneknya yang sakit keras, dengan motif tersembunyi untuk menjadi pewaris tunggal harta kekayaannya, namun ia justru menemukan hal yang lebih berharga daripada uang.",
+            genres: ["Drama", "Family"],
+            poster: "./Asset/PosterFilm/Before grandma dies poster.jpg",
+            background: "./Asset/BackgroundFilm/Before grandma diesL.jpeg", 
+            trailerId: "0lpXPWKpSY4",
+            filmId: "0lpXPWKpSY4"
+        },
+        {
+        id: 15,
+        title: "The Shining", 
+        year: "1980", 
+        duration: "2h 26m",
+        rating: "8.4/10", 
+        desc: "Jack Torrance menerima pekerjaan sebagai penjaga musim dingin di Hotel Overlook yang terisolasi, namun kehadiran roh jahat di hotel tersebut perlahan mendorongnya ke dalam kegilaan dan membahayakan istri serta putranya.",
+        genres: ["Horror", "Drama"],
+        poster: "./Asset/PosterFilm/theShining.jpg",
+        background: "./Asset/BackgroundFilm/theShiningL.jpg", 
+        trailerId: "S014oGZiSdI",
+        filmId: "S014oGZiSdI"
+        },
+        {
+            id: 16,
+            title: "Titanic", 
+            year: "1997", 
+            duration: "3h 14m", 
+            rating: "7.9/10", 
+            desc: "Seorang aristokrat berusia 17 tahun jatuh cinta dengan seniman miskin namun baik hati di atas kapal R.M.S. Titanic yang mewah namun bernasib malang.",
+            genres: ["Drama", "Romance"],
+            poster: "./Asset/PosterFilm/Titanic.jpeg",
+            background: "./Asset/BackgroundFilm/TitanicL.jpeg", 
+            trailerId: "CHekzSiZjrY",
+            filmId: "CHekzSiZjrY"
+        },
+        
+
+
     ];
 
     /* =========================================
@@ -88,7 +227,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const heroRating = document.getElementById('heroRating');
     const heroGenres = document.getElementById('heroGenres');
     const heroPoster = document.getElementById('heroPoster');
-    const heroVideo = document.getElementById('heroVideo'); // Iframe Background
+    const heroVideo = document.getElementById('heroVideo'); 
+    const heroDuration = document.getElementById('heroDuration'); 
     
     const trailerBtn = document.getElementById('btnTrailer');
     const playBtn = document.getElementById('btnPlay');
@@ -105,6 +245,7 @@ document.addEventListener('DOMContentLoaded', () => {
         heroTitle.textContent = movie.title;
         heroDesc.textContent = movie.desc;
         heroYear.textContent = movie.year;
+        heroDuration.textContent = movie.duration;
         heroRating.textContent = movie.rating;
         heroPoster.src = movie.poster;
         
